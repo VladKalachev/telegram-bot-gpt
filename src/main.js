a47879689211a48742fd8854da04aee67f48723c
Corvax <vlvete1990@yandex.ru>
2024-05-13 20:58:57 +0300

    feature: base bot

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log("Starting...")
+import { Telegraf } from 'telegraf';
+import config from 'config';
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
