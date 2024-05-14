a6d6184a450c5ff4f5cb39c8dfd014a5a0a2ee38
Corvax <vlvete1990@yandex.ru>
2024-05-14 12:44:58 +0300

    feature: add text get message

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, session } from 'telegraf';
 import { message } from 'telegraf/filters';
 import config from 'config';
 import { ogg } from './ogg.js';
 import { openai } from './openai.js';
 
+const INITIAL_SESSION = {
+  messages: []
+}
+
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
+bot.use(session())
+
+bot.command('new', async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("Жду вашего голосового или текстового сообщения")
+})
+
+bot.command('start', async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("Жду вашего голосового или текстового сообщения")
+})
+
+
 bot.on(message('voice'), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
   try {
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
     const userId = String(ctx.message.from.id);
@@ -14,17 +32,33 @@ bot.on(message('voice'), async (ctx) => {
     const mp3Path = await ogg.toMp3(oggPath, userId);
 
     const text = await openai.transcription(mp3Path);
-    const response = await openai.chat(text);
 
-    await ctx.reply(response);
+    ctx.session.messages.push([{ role: openai.roles.USER, content: text}]);
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push([{ role: openai.roles.ASSISTANT, content: response.content }]);
+
+    await ctx.reply(response.content);
   } catch (error) {
     console.log(error);
   }
 });
 
-bot.command('start', async (ctx) => {
-  await ctx.reply('Hello')
-})
+bot.on(message('text'), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    const text = await openai.transcription(ctx.message.text);
+
+    ctx.session.messages.push([{ role: openai.roles.USER, content: text}]);
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push([{ role: openai.roles.ASSISTANT, content: response.content }]);
+
+    await ctx.reply(response.content);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 bot.launch();
 
--- src/openai.js
+++ src/openai.js
@@ -1,13 +1,42 @@
+import OpenAI from 'openai';
+import config from 'config';
+import { createReadStream } from 'fs'
 class OpenAi {
-  constructor() {}
-
-  toMp3() {
-
+  roles = {
+    ASSISTANT: 'assistant',
+    USER: 'user',
+    SYSTEM: 'system',
   }
 
-  create() {
-    
+  constructor(apiKey) {
+    this.openai = new OpenAI({
+      apiKey,
+    });
+  }
+
+  async chat(messages) {
+    try {
+      const response = await this.openai.chat.completions({
+        module: 'gpt-3.5-turbo',
+        messages
+      });
+      return response.data.choices[0].message;
+    } catch (error) {
+      console.log('Error while chat', error.message);
+    }
+  }
+
+  async transcription(filepath) {
+    try {
+      const response = await this.openai.audio.transcriptions.create({
+        file: createReadStream(filepath),
+        model: 'whisper-1'
+      });
+      return response.data.text;
+    } catch (error) {
+      console.log('Error while transcription', error.message);
+    }
   }
 }
 
-export const openai = new OpenAi();
+export const openai = new OpenAi(config.get('OPENAI_API_KEY'));
